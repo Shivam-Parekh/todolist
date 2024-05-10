18b1e224795f0b7bca378c5e59aac27159fcfda7
Shivam-Parekh <sparekh1248@gmail.com>
2024-05-10 00:03:17 -0400

    Added eslinter and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,52 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <button>Create a note</button>
-        <hr
-        style={{
-            color: 'white',
-            backgroundColor: 'blue',
-            height: 5
-        }}
-        />
-        <div className="Note-Column">
-        <div>
-        <li>
-        <div className="Note-Item New">
-          <p>HELLO HERE</p>
-        </div>
-        <div className="Note-Item New">
-          <p>HELLO HERE 2</p>
-        </div>
-        </li>
-        </div>
-        <div>
-        <li>
-        <div className="Note-Item Progress">
-          <p>HELLO HERE</p>
-        </div>
-        <div className="Note-Item Progress">
-          <p>HELLO HERE 2</p>
-        </div>
-        </li>
-        </div>
-        <div>
-        <li>
-        <div className="Note-Item Complete">
-          <p>HELLO HERE</p>
-        </div>
-        <div className="Note-Item Complete">
-          <p>HELLO HERE 2</p>
-        </div>
-        </li>
-        </div>
-        </div>
-      </header>
+      <Container>
+        <header className="App-header">
+          <Row>
+            <button>Create a note</button>
+            <hr
+              style={{
+                color: 'white',
+                backgroundColor: 'blue',
+                height: 5
+              }}
+            />
+          </Row>
+          <Row>
+            <div className="Note-Column">
+              <Col>
+                <div>
+                  <li>
+                    <div className="Note-Item New">
+                      <p>HELLO HERE</p>
+                    </div>
+                    <div className="Note-Item New">
+                      <p>HELLO HERE 2</p>
+                    </div>
+                  </li>
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <li>
+                    <div className="Note-Item Progress">
+                      <p>HELLO HERE</p>
+                    </div>
+                    <div className="Note-Item Progress">
+                      <p>HELLO HERE 2</p>
+                    </div>
+                  </li>
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <li>
+                    <div className="Note-Item Complete">
+                      <p>HELLO HERE</p>
+                    </div>
+                    <div className="Note-Item Complete">
+                      <p>HELLO HERE 2</p>
+                    </div>
+                  </li>
+                </div>
+              </Col>
+            </div>
+          </Row>
+        </header>
+      </Container>
     </div>
   );
 }
