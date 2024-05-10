e568c7330d1e31e90972dc68af8923fc11d1c7f8
Shivam-Parekh <sparekh1248@gmail.com>
2024-05-09 23:00:50 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Create a note</button>
+        <hr
+        style={{
+            color: 'white',
+            backgroundColor: 'blue',
+            height: 5
+        }}
+        />
+        <div className="Note-Column">
+        <div>
+        <li>
+        <div className="Note-Item New">
+          <p>HELLO HERE</p>
+        </div>
+        <div className="Note-Item New">
+          <p>HELLO HERE 2</p>
+        </div>
+        </li>
+        </div>
+        <div>
+        <li>
+        <div className="Note-Item Progress">
+          <p>HELLO HERE</p>
+        </div>
+        <div className="Note-Item Progress">
+          <p>HELLO HERE 2</p>
+        </div>
+        </li>
+        </div>
+        <div>
+        <li>
+        <div className="Note-Item Complete">
+          <p>HELLO HERE</p>
+        </div>
+        <div className="Note-Item Complete">
+          <p>HELLO HERE 2</p>
+        </div>
+        </li>
+        </div>
+        </div>
       </header>
     </div>
   );
